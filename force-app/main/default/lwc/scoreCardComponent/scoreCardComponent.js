efb67e6b7dbcd965a2b7da6a275f4169c8897858
diarmuidhiggins1 <diarmuid.higgins@boi.com>
2024-09-24 16:06:01 +0100

    lwc changes to scorecard cmp and add to golf round flexi page

--- force-app/main/default/lwc/scoreCardComponent/scoreCardComponent.js
+++ force-app/main/default/lwc/scoreCardComponent/scoreCardComponent.js
@@ -1,4 +1,73 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire, api } from "lwc";
+import getAllScores from "@salesforce/apex/GolfScorecardController.getAllScores";
+import { refreshApex } from "@salesforce/apex";
+import { updateRecord } from "lightning/uiRecordApi";
 
-export default class ScoreCardComponent extends LightningElement {}
- 
+import { ShowToastEvent } from "lightning/platformShowToastEvent";
+import HOLENUMBERFIELD from "@salesforce/schema/Golf_Round_Score__c.Hole_Number__c";
+import PAROFHOLE from "@salesforce/schema/Golf_Round_Score__c.Par_of_Hole__c";
+import SCOREOFHOLE from "@salesforce/schema/Golf_Round_Score__c.Score__c";
+import PHONE_FIELD from "@salesforce/schema/Contact.Phone";
+import EMAIL_FIELD from "@salesforce/schema/Contact.Email";
+import ID_FIELD from "@salesforce/schema/Contact.Id";
+
+const COLS = [
+  {
+    label: "Hole Number",
+    fieldName: HOLENUMBERFIELD.fieldApiName,
+    editable: true
+  },
+  {
+    label: "Par of Hole",
+    fieldName: PAROFHOLE.fieldApiName,
+    editable: true
+  },
+  { label: "Score", fieldName: SCOREOFHOLE.fieldApiName, editable: true }
+
+];
+
+export default class scoreCardComponent extends LightningElement {
+  @api recordId;
+  columns = COLS;
+  draftValues = [];
+
+  @wire(getAllScores, { golfRoundId: "$recordId" })
+  scores;
+
+  async handleSave(event) {
+    // Convert datatable draft values into record objects
+    const records = event.detail.draftValues.slice().map((draftValue) => {
+      const fields = Object.assign({}, draftValue);
+      return { fields };
+    });
+
+    // Clear all datatable draft values
+    this.draftValues = [];
+
+    try {
+      // Update all records in parallel thanks to the UI API
+      const recordUpdatePromises = records.map((record) => updateRecord(record));
+      await Promise.all(recordUpdatePromises);
+
+      // Report success with a toast
+      this.dispatchEvent(
+        new ShowToastEvent({
+          title: "Success",
+          message: "scores updated",
+          variant: "success"
+        })
+      );
+
+      // Display fresh data in the datatable
+      await refreshApex(this.scores);
+    } catch (error) {
+      this.dispatchEvent(
+        new ShowToastEvent({
+          title: "Error updating or reloading scores",
+          message: error.body.message,
+          variant: "error"
+        })
+      );
+    }
+  }
+}
