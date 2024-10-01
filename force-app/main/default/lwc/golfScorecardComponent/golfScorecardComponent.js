4c093f87165db65034e31708eb16436dfab17b4b
diarmuidhiggins1 <diarmuid.higgins@boi.com>
2024-10-01 09:19:13 +0100

    enhanced logic

---
+++ force-app/main/default/lwc/golfScorecardComponent/golfScorecardComponent.js
@@ -0,0 +1,159 @@
+import { LightningElement, track, wire, api } from 'lwc';
+import fetchScores from '@salesforce/apex/GolfScorecardController.getAllScores';
+import SCORE_OBJECT from '@salesforce/schema/Golf_Round_Score__c';
+import SCORE_FIELD from '@salesforce/schema/Golf_Round_Score__c.Score__c';
+import { updateRecord } from 'lightning/uiRecordApi';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import { refreshApex } from '@salesforce/apex';
+import { getPicklistValues, getObjectInfo } from 'lightning/uiObjectInfoApi';
+ //Hole_Number__c, Par_of_Hole__c, Score__c,Hole_Distance_Yards__c
+const columns = [
+    { label: 'Hole', fieldName: 'Hole_Number__c', editable: true },
+    { label: 'Par', fieldName: 'Par_of_Hole__c', type: 'text', editable: true },
+    {
+        label: 'Score', fieldName: 'Score__c', type: 'picklistColumn', editable: true, typeAttributes: {
+            placeholder: 'Choose Score', options: { fieldName: 'pickListOptions' }, 
+            value: { fieldName: 'Score__c' }, // default value for picklist,
+            context: { fieldName: 'Id' } // binding account Id with context variable to be returned back
+        }
+    },
+    { label: 'Distance(Yrds)', fieldName: 'Hole_Distance_Yards__c', type: 'text', editable: true },
+]
+ 
+export default class CustomDatatableDemo extends LightningElement {
+    @api recordId;
+    columns = columns;
+    showSpinner = false;
+    @track data = [];
+    @track accountData;
+    @track draftValues = [];
+    lastSavedData = [];
+    @track pickListOptions;
+ 
+    @wire(getObjectInfo, { objectApiName: SCORE_OBJECT })
+    objectInfo;
+ 
+    //fetch picklist options
+    @wire(getPicklistValues, {
+        recordTypeId: "$objectInfo.data.defaultRecordTypeId",
+        fieldApiName: SCORE_FIELD
+    })
+ 
+    wirePickList({ error, data }) {
+        if (data) {
+            this.pickListOptions = data.values;
+        } else if (error) {
+            console.log(error);
+        }
+    }
+ 
+    //here I pass picklist option so that this wire method call after above method
+    @wire(fetchScores, { golfRoundId: "$recordId", pickList: '$pickListOptions' })
+    //@wire(fetchScores, { golfRoundId: "$recordId" })
+    accountData(result) {
+        this.accountData = result;
+        if (result.data) {
+            this.data = JSON.parse(JSON.stringify(result.data));
+ 
+            this.data.forEach(ele => {
+                ele.pickListOptions = this.pickListOptions;
+            })
+ 
+            this.lastSavedData = JSON.parse(JSON.stringify(this.data));
+ 
+        } else if (result.error) {
+            this.data = undefined;
+        }
+    };
+ 
+    updateDataValues(updateItem) {
+        let copyData = JSON.parse(JSON.stringify(this.data));
+ 
+        copyData.forEach(item => {
+            if (item.Id === updateItem.Id) {
+                for (let field in updateItem) {
+                    item[field] = updateItem[field];
+                }
+            }
+        });
+ 
+        //write changes back to original data
+        this.data = [...copyData];
+    }
+ 
+    updateDraftValues(updateItem) {
+        let draftValueChanged = false;
+        let copyDraftValues = [...this.draftValues];
+        //store changed value to do operations
+        //on save. This will enable inline editing &
+        //show standard cancel & save button
+        copyDraftValues.forEach(item => {
+            if (item.Id === updateItem.Id) {
+                for (let field in updateItem) {
+                    item[field] = updateItem[field];
+                }
+                draftValueChanged = true;
+            }
+        });
+ 
+        if (draftValueChanged) {
+            this.draftValues = [...copyDraftValues];
+        } else {
+            this.draftValues = [...copyDraftValues, updateItem];
+        }
+    }
+ 
+    //handler to handle cell changes & update values in draft values
+    handleCellChange(event) {
+        //this.updateDraftValues(event.detail.draftValues[0]);
+        let draftValues = event.detail.draftValues;
+        draftValues.forEach(ele=>{
+            this.updateDraftValues(ele);
+        })
+    }
+ 
+    handleSave(event) {
+        this.showSpinner = true;
+        this.saveDraftValues = this.draftValues;
+ 
+        const recordInputs = this.saveDraftValues.slice().map(draft => {
+            const fields = Object.assign({}, draft);
+            return { fields };
+        });
+ 
+        // Updateing the records using the UiRecordAPi
+        const promises = recordInputs.map(recordInput => updateRecord(recordInput));
+        Promise.all(promises).then(res => {
+            this.showToast('Success', 'Records Updated Successfully!', 'success', 'dismissable');
+            this.draftValues = [];
+            return this.refresh();
+        }).catch(error => {
+            console.log(error);
+            this.showToast('Error', 'An Error Occured!!', 'error', 'dismissable');
+        }).finally(() => {
+            this.draftValues = [];
+            this.showSpinner = false;
+        });
+    }
+ 
+    handleCancel(event) {
+        //remove draftValues & revert data changes
+        this.data = JSON.parse(JSON.stringify(this.lastSavedData));
+        this.draftValues = [];
+    }
+ 
+    showToast(title, message, variant, mode) {
+        const evt = new ShowToastEvent({
+            title: title,
+            message: message,
+            variant: variant,
+            mode: mode
+        });
+        this.dispatchEvent(evt);
+    }
+ 
+    // This function is used to refresh the table once data updated
+    async refresh() {
+        await refreshApex(this.accountData);
+    }
+}
